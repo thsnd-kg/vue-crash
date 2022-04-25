21108e1a929cfcd88cf54ffda1ad0b974d0004c8
Phạm Tấn khang <phamtankhang2001@gmail.com>
2022-04-25 09:15:18 +0700

    .

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      :color="showAddTask ? 'red' : 'green'"
+      @toggle-add-task="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+    />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  props: {
+    title: {
+      type: String,
+    },
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+
+  emits: ["toggle-add-task"],
+};
+</script>
+
+
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
---
+++ src/components/Task.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    :class="[task.reminder ? 'reminder' : '', 'task']"
+    @dblclick="$emit('toggle-reminder')"
+  >
+    <h3>
+      {{ task.text }}
+      <i @click="$emit('delete-task')" class="fas fa-times"></i>
+    </h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Task",
+  props: {
+    task: Object,
+  },
+};
+</script>
+
+<style scoped>
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.reminder {
+  border-left: 5px solid green;
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.fas {
+  color: red;
+}
+</style>
